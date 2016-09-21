b150331b413f7940c3f11065ed1daeba3f372e3b
A Parks <parksa@seas.upenn.edu>
2016-09-21 15:17:14 -0500

    Create demo-plugin.js

---
+++ js/demo-plugin.js
@@ -0,0 +1,64 @@
+(function(prefix, wizehive, angular) {
+
+	// Get plugin registration object and protect important items within plugin code
+	var plugin = wizehive.makePlugin(prefix);
+	wizehive = null;
+
+	angular = angular.extend({}, angular);
+	delete angular.module;
+	delete angular.injector;
+	delete angular.bootstrap;
+
+	var window = null,
+		SnapEngage = null,
+		UserVoice = null,
+		_cio = null,
+		userEvent = null;
+	
+	
+
+/**
+ * Demo Controller
+ */
+plugin.controller('demoPluginCntl', ['$scope', 'znPluginData', function ($scope, pd) {
+	
+	$scope.text = 'Hello World!';
+
+}])
+
+/**
+ * Demo Settings Controller
+ */
+.controller('demoPluginSettingsCntl', ['$scope', function ($scope) {
+	
+	$scope.text = 'Hello Settings World!';
+
+}])
+
+/**
+ * Plugin Registration
+ */
+.register('demo-plugin', {
+	route: '/demo-plugin',
+	title: 'Demo Plugin',
+	icon: 'icon-puzzle',
+	interfaces: [
+		{
+			controller: 'demoPluginCntl',
+			template: 'demo-plugin-main',
+			type: 'fullPage',
+			order: 300,
+			topNav: true,
+			routes: [
+				'/:page'
+			]
+		},
+		{
+			controller: 'demoPluginSettingsCntl',
+			template: 'demo-plugin-settings',
+			type: 'settings'
+		}
+	]
+});
+ 
+})("hipchat5", wizehive, angular);
