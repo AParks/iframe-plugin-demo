6bac14e93cea3f7191f1ec07df39bd1ed60e9912
A Parks <parksa@seas.upenn.edu>
2016-09-22 10:50:58 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,7 +8,8 @@ angular.module('wizehive', []).controller('AppCntl', ['$scope', '$http', '$templ
 			controller: 'demoPluginCntl',
 			template: 'demo-plugin-main'
 		}
-	    wizehive.formatHtml(raw_html, pluginContext, $templateCache);
+		console.log(response.data);
+	    wizehive.formatHtml(response.data, pluginContext, $templateCache);
 	});
 
 	console.log('main controller');
