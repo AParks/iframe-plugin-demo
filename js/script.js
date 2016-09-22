e1bc313e53dba0fe6d274befd4e5d5d28e1a1132
A Parks <parksa@seas.upenn.edu>
2016-09-22 10:49:20 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
-angular.module('wizehive', []).controller('AppCntl', ['$scope', function(
-		$scope
+angular.module('wizehive', []).controller('AppCntl', ['$scope', '$http', '$templateCache', function(
+		$scope, $http, $templateCache
 ) {
+
+	$http.get('/iframe-plugin-demo/plugin/plugin.html').then(function(response) {
+		var pluginContext = {
+			prefix: 'demoPlugin',
+			controller: 'demoPluginCntl',
+			template: 'demo-plugin-main'
+		}
+	    wizehive.formatHtml(raw_html, pluginContext, $templateCache);
+	});
 
 	console.log('main controller');
 
