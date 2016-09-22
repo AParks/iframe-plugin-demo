d86ef16d22f8b071d9b6f614ff1f236d14390d26
A Parks <parksa@seas.upenn.edu>
2016-09-22 15:33:36 -0500

    Create core.js

---
+++ js/core.js
@@ -0,0 +1,34 @@
+(function(wizehive, global) {
+	Function.prototype.curry = Function.prototype.curry || function () {
+		var fn = this, args = Array.prototype.slice.call(arguments);
+		return function () {
+			return fn.apply(this, args.concat(Array.prototype.slice.call(arguments)));
+		};
+	};
+
+	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/startsWith
+	if (!String.prototype.startsWith) {
+		String.prototype.startsWith = function(searchString, position){
+			position = position || 0;
+			return this.substr(position, searchString.length) === searchString;
+		};
+	}
+
+	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
+	if (!String.prototype.endsWith) {
+		String.prototype.endsWith = function(searchString, position) {
+			var subjectString = this.toString();
+			if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
+				position = subjectString.length;
+			}
+			position -= searchString.length;
+			var lastIndex = subjectString.indexOf(searchString, position);
+			return lastIndex !== -1 && lastIndex === position;
+		};
+	}
+
+	String.prototype.splice = String.prototype.splice || function(idx, deleteCount, value) {
+    	return (this.slice(0,idx) + value + this.slice(idx + Math.abs(deleteCount)));
+	};
+	
+})(wizehive, window);
